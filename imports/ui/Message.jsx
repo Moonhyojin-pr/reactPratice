b3de2e0f4c63200a60fc09656e578a66b02d8d5e
kakadais <kakadais@gmail.com>
2024-09-19 17:17:56 +0900

    - 추가 샘플 코드들.

--- imports/ui/Message.jsx
+++ imports/ui/Message.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { CollectionPosts } from "/imports/api/collections.js";
+import MessageInput from "./MessageInput.jsx";
 // message inputbox
 // import { useState } from 'react'
 // import {
@@ -131,75 +134,83 @@ import React from 'react'
 // )
 // }
 
+// const comments = [
+//     {
+//       id: 1,
+//       name: 'Leslie Alexander',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//       content:
+//         'Explicabo nihil laborum. Saepe facilis consequuntur in eaque. Consequatur perspiciatis quam. Sed est illo quia. Culpa vitae placeat vitae. Repudiandae sunt exercitationem nihil nisi facilis placeat minima eveniet.',
+//       date: '1d ago',
+//       dateTime: '2023-03-04T15:54Z',
+//     },
+//     {
+//       id: 2,
+//       name: 'Michael Foster',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//       content:
+//         'Laudantium quidem non et saepe vel sequi accusamus consequatur et. Saepe inventore veniam incidunt cumque et laborum nemo blanditiis rerum. A unde et molestiae autem ad. Architecto dolor ex accusantium maxime cumque laudantium itaque aut perferendis.',
+//       date: '2d ago',
+//       dateTime: '2023-03-03T14:02Z',
+//     },
+//     {
+//       id: 3,
+//       name: 'Dries Vincent',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//       content:
+//         'Quia animi harum in quis quidem sint. Ipsum dolorem molestias veritatis quis eveniet commodi assumenda temporibus. Dicta ut modi alias nisi. Veniam quia velit et ut. Id quas ducimus reprehenderit veniam fugit amet fugiat ipsum eius. Voluptas nobis earum in in vel corporis nisi.',
+//       date: '2d ago',
+//       dateTime: '2023-03-03T13:23Z',
+//     },
+//     {
+//       id: 4,
+//       name: 'Lindsay Walton',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//       content:
+//         'Unde dolore exercitationem nobis reprehenderit rerum corporis accusamus. Nemo suscipit temporibus quidem dolorum. Nobis optio quae atque blanditiis aspernatur doloribus sit accusamus. Sunt reiciendis ut corrupti ab debitis dolorem dolorem nam sit. Ducimus nisi qui earum aliquam. Est nam doloribus culpa illum.',
+//       date: '3d ago',
+//       dateTime: '2023-03-02T21:13Z',
+//     },
+//   ]
 
+export default function Message() {
+  // const [messages, setMessages] = useState([])
+  // useEffect(() => {
+  //   json = fetch(...,)
+  //   json....
+  //   setMessages(json)
+  // }, [])
 
-const comments = [
-    {
-      id: 1,
-      name: 'Leslie Alexander',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Explicabo nihil laborum. Saepe facilis consequuntur in eaque. Consequatur perspiciatis quam. Sed est illo quia. Culpa vitae placeat vitae. Repudiandae sunt exercitationem nihil nisi facilis placeat minima eveniet.',
-      date: '1d ago',
-      dateTime: '2023-03-04T15:54Z',
-    },
-    {
-      id: 2,
-      name: 'Michael Foster',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Laudantium quidem non et saepe vel sequi accusamus consequatur et. Saepe inventore veniam incidunt cumque et laborum nemo blanditiis rerum. A unde et molestiae autem ad. Architecto dolor ex accusantium maxime cumque laudantium itaque aut perferendis.',
-      date: '2d ago',
-      dateTime: '2023-03-03T14:02Z',
-    },
-    {
-      id: 3,
-      name: 'Dries Vincent',
-      imageUrl:
-        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Quia animi harum in quis quidem sint. Ipsum dolorem molestias veritatis quis eveniet commodi assumenda temporibus. Dicta ut modi alias nisi. Veniam quia velit et ut. Id quas ducimus reprehenderit veniam fugit amet fugiat ipsum eius. Voluptas nobis earum in in vel corporis nisi.',
-      date: '2d ago',
-      dateTime: '2023-03-03T13:23Z',
-    },
-    {
-      id: 4,
-      name: 'Lindsay Walton',
-      imageUrl:
-        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Unde dolore exercitationem nobis reprehenderit rerum corporis accusamus. Nemo suscipit temporibus quidem dolorum. Nobis optio quae atque blanditiis aspernatur doloribus sit accusamus. Sunt reiciendis ut corrupti ab debitis dolorem dolorem nam sit. Ducimus nisi qui earum aliquam. Est nam doloribus culpa illum.',
-      date: '3d ago',
-      dateTime: '2023-03-02T21:13Z',
-    },
-  ]
-  
-  
-  export default function Message() {
-    return (
+  const messages = useTracker(() => {
+    return CollectionPosts.find({}, { sort: { createdAt: -1 } }).fetch();
+  }, []);
 
-
+  return (
+    <>
+      <MessageInput />
       <ul role="list" className="divide-y divide-gray-100">
-        {comments.map((comment) => (
-          <li key={comment.id} className="flex gap-x-4 py-5">
-            <img alt="" src={comment.imageUrl} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
+        {messages.map((message) => (
+          <li key={message._id} className="flex gap-x-4 py-5">
+            {/* <img alt="" src={message.imageUrl} className="h-12 w-12 flex-none rounded-full bg-gray-50" /> */}
             <div className="flex-auto">
               <div className="flex items-baseline justify-between gap-x-4">
                 {/* name, date */}
-                {/* <p className="text-sm font-semibold leading-6 text-gray-900">{comment.name}</p> */}
+                {/* <p className="text-sm font-semibold leading-6 text-gray-900">{message.name}</p> */}
                 {/* <p className="flex-none text-xs text-gray-600">
-                  <time dateTime={comment.dateTime}>{comment.date}</time>
+                  <time dateTime={message.dateTime}>{message.date}</time>
                 </p> */}
               </div>
-              <p className="mt-1 line-clamp-2 text-sm leading-6 text-gray-600">{comment.content}</p>
+              <p className="mt-1 line-clamp-2 text-sm leading-6 text-gray-600">
+                {message.message} / {message.createdAt.toLocaleString()}
+              </p>
             </div>
           </li>
         ))}
       </ul>
-
-      
-    )
-  }
-
+    </>
+  );
+}
